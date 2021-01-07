2158bedf98193b6dc299a5a8a26f523fc541375c
RamenWhite <meganenormand1@gmail.com>
2021-01-06 23:00:51 -0500

    added the MobBoss and added the basic of the turn concept

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from random import randint
 
 class Minions:
-    def __init__ (self, health, blue, red, green):
+    def __init__ (self, health, red, green, blue):
         self.health = health
         self.blue = blue
         self.red = red
         self.green = green
-        self.component = [health, blue, red, green]
-        self.componentName = ['health', 'blue', 'red', 'green']
+        self.component = [health, red, green, blue]
+        self.componentName = ['health', 'red', 'green', 'blue']
         self._index = -1
     
+    def get_HRGB(self):
+        return (
+            {
+                'health' : self.health,
+                'red' : self.red,
+                'green' : self.green,
+                'blue' : self.blue
+            }
+        )
+
     def __iter__(self):
         return self
 
@@ -24,20 +34,100 @@ class Minions:
 class MinionsListClass:
     def __init__ (self):
         self.guild = []
+        self._index = -1
     
     def create_minion(self):
-        health = randint(1,50)
-        blue = randint(5, 50)
-        red = randint(5, 50)
-        green = randint(5, 50)
-        minion = Minions(health, blue, red, green)
+        health = 20 #Every minions has the same number of HP
+        blue = randint(1, 15)
+        red = randint(1, 15)
+        green = randint(1, 15)
+        totalPoint = blue + red + green
+
+        supposedTotalPoint = 50
+        blue = int(blue * (supposedTotalPoint / totalPoint)) #50 is the supposed total point of a minion
+        red = int(red * (supposedTotalPoint / totalPoint))
+        green = int(green * (supposedTotalPoint / totalPoint))
+
+        listElement = [red, green, blue] #to get the position of the largest element RGB
+        positionOfSmallest = listElement.index(min(red, green, blue))
+        difference = (red + green + blue) - supposedTotalPoint
+        listElement[positionOfSmallest] -= difference
+        minion = Minions(health, listElement[0], listElement[1], listElement[2])
         self.guild.append(minion)
 
-MinionsList = MinionsListClass()
-MinionsList.create_minion()
-listMinions = MinionsList.guild
-for i in list(MinionsList.guild[0]):
-    print (i)
+    def __iter__(self):
+        return(self)
+    
+    def __next__(self):
+        self._index += 1
+        if self._index >= len(self.guild):
+            self._index -= 1
+            raise StopIteration
+        else:
+            return(guild[self._index])
 
-print(MinionsList.guild[0])
+class TurnFlow:
+    def __init__(self, guild, mob):
+        self.guild = guild
+        self.mob = mob
+        self.index = 0
+    
+    def simpleCombat(self):
+        currentMinion = self.guild[self.index]
+
+class MobBossClass:
+    def __init__(self):
+        self.health = int
+        self.red = int
+        self.green = int
+        self.blue = int
+        self.listElement = list
+
+    def RGB_determination(self):
+        blue = randint(3, 20)
+        red = randint(3, 20)
+        green = randint(3, 20)
+        totalPoint = blue + red + green
+
+        supposedTotalPoint = 65
+        blue = int(blue * (supposedTotalPoint / totalPoint)) #65 is the supposed total point for the boss
+        red = int(red * (supposedTotalPoint / totalPoint))
+        green = int(green * (supposedTotalPoint / totalPoint))
+
+        self.listElement = [red, green, blue] #to get the position of the largest element RGB
+        positionOfSmallest = self.listElement.index(min(red, green, blue))
+        difference = (red + green + blue) - supposedTotalPoint 
+        self.listElement[positionOfSmallest] -= difference
+
+    def execute_creation(self):
+        self.RGB_determination()
+        self.health = 1000
+        self.red = self.listElement[0]
+        self.green = self.listElement[1]
+        self.blue = self.listElement[2]
+
+    def getHRGB(self):
+        return ( 
+            { 
+                'health' : self.health,
+                'red' : self.red,
+                'green' : self.green,
+                'blue' : self.blue
+            }
+        )
+MinionsList = MinionsListClass()
+for e in range(50):
+    MinionsList.create_minion()
+
+index = 0
+for j in MinionsList.guild:
+    index += 1
+    print ("\nMinion #" + str(index))
+    print(j.get_HRGB())
+
+MobBoss = MobBossClass()
+MobBoss.execute_creation()
+print('\nMobBoss stats\n', MobBoss.getHRGB())
+
+
 pass
