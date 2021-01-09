423c4c5e06f56c7c4fb25485e263ff47c34aa112
Binimows <cricorafi@gmail.com>
2021-01-09 18:00:51 -0500

    Health basic - Next : Prettier logs

--- TurnFlow.py
+++ TurnFlow.py
@@ -5,24 +5,33 @@ class TurnFlow:
         self.index = 0
     
     def simpleCombat(self, currentMinion):
-        allResult =  {   
-            'redResult' : currentMinion.red - self.mob.red,
-            'greenResult' : currentMinion.green - self.mob.green,
-            'blueResult' : currentMinion.blue - self.mob.blue
-        }
 
-        outPerTurn = 0 #Dmg dealt each turn
-        inPerTurn = 0 #Dmg received each turn
-        for i in allResult:
-            if allResult[i] >= 0:
-                outPerTurn += allResult[i]
-                print("Damage out : " + str(allResult[i]))
-            else:
-                inPerTurn += allResult[i]
-                print("Damage in : " + str(allResult[i]))
-    
+        while currentMinion.health > 0:
+            allResult =  {   
+                'redResult' : currentMinion.red - self.mob.red,
+                'greenResult' : currentMinion.green - self.mob.green,
+                'blueResult' : currentMinion.blue - self.mob.blue
+            }
+
+            outPerTurn = 0 #Dmg dealt each turn
+            inPerTurn = 0 #Dmg received each turn
+            for i in allResult:
+                if allResult[i] <= 0:
+                    inPerTurn += allResult[i]
+                    currentMinion.health += inPerTurn
+                    print("The minion now have ", currentMinion.health)
+                    print("Damage in : " + str(allResult[i]))
+                    if currentMinion.health <= 0:
+                        break
+                else:
+                    outPerTurn += allResult[i]
+                    self.mob.health -= outPerTurn
+                    print("Damage out : " + str(allResult[i]))
+                    
+        
     def followingCombats(self):
         for i in self.guild:
             currentMinion = self.guild[self.index]
             self.simpleCombat(currentMinion)
+            print ("A minion died : ", self.index)
             self.index += 1
